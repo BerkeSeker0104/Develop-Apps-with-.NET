a1aebef20db728161b4e23f7caba1194698a9c12
Berke Seker <berke.seker@std.yildiz.edu.tr>
2024-08-21 21:37:20 +0300

    C#_101 eklendi

---
+++ C#_101/Program.cs
@@ -0,0 +1,13 @@
+﻿//Console.WriteLine("Hello World!");
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
+
+Console.WriteLine("Congratulations!");
+Console.Write("You wrote your first lines of code.");
+
+Console.WriteLine("This is the first line.");
+
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
